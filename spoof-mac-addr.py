10297e8f5da2659729780e7ec71fad896da96abb
nick3499 <ntrinen@yahoo.com>
2021-05-07 20:23:32 -0500

    revised code
    - `from random import randrange as rr` was `from random import randint as ri`
    - `with open('csv/oui.csv') as csv_file:` was `with open('csv/oui.csv') as csv_f:`
    - `csv_r = reader(csv_f)` was `csv_reader = reader(csv_file)`
    - `oui_list = []` was `oui_l = []`
    - `for rec in csv_reader:` was `for rec in csv_r:`
    - `oui_list.append(rec[0])` was `oui_l.append(rec[0])`
    - `oui_h = oui_list[ri(0, len(oui_list))]` was `oui_h = oui_l[rr(0, len(oui_l))]`
    - `mac_rgt = f"{hex(ri(16, 256))[2:]}:{hex(ri(16, 256))[2:]}:{hex(ri(16, 256))[2:]}"` was `mac_rgt = f"{hex(rr(16, 256))[2:]}:{hex(rr(16, 256))[2:]}:{hex(rr(16, 256))[2:]}"`

--- spoof-mac-addr.py
+++ spoof-mac-addr.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
 '''Spoof MAC address. (as a cybersecurity enhancement)'''
 from csv import reader
-from random import randrange as rr
+from random import randint as ri
 from subprocess import run
 
 
-with open('csv/oui.csv') as csv_f:
-    csv_r = reader(csv_f)
-    oui_l = []
-    for rec in csv_r:
-        oui_l.append(rec[0])
-    oui_h = oui_l[rr(0, len(oui_l))]
+with open('csv/oui.csv') as csv_file:
+    csv_reader = reader(csv_file)
+    oui_list = []
+    for rec in csv_reader:
+        oui_list.append(rec[0])
+    oui_h = oui_list[ri(0, len(oui_list))]
     oui = ':'.join(oui_h.split('-'))
 
-mac_rgt = f"{hex(rr(16, 256))[2:]}:{hex(rr(16, 256))[2:]}:{hex(rr(16, 256))[2:]}"
+mac_rgt = f"{hex(ri(16, 256))[2:]}:{hex(ri(16, 256))[2:]}:{hex(ri(16, 256))[2:]}"
+
+print(f"{oui}:{mac_rgt}")
 
 run(["sudo", "ip", "link", "set", "enp2s0", "address", f"{oui}:{mac_rgt}"], check=True)
-
